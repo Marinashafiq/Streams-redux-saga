6e569cab61544e32a96c6e60b0573fcce04d28b6
Marina Magdy <Marina.Magdy@ibtikar.com>
2019-12-30 13:33:51 +0200

    Updates

---
+++ precache-manifest.db51e76abec0492ade273419bd6efef0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82f6f9fe416e5611be52db5e7e614d04",
+    "url": "/streams-redux-saga/index.html"
+  },
+  {
+    "revision": "9ddd8d7ef92390950143",
+    "url": "/streams-redux-saga/static/js/2.25ef9a5a.chunk.js"
+  },
+  {
+    "revision": "7d1d1b7edb4391a0d47f",
+    "url": "/streams-redux-saga/static/js/3.960be541.chunk.js"
+  },
+  {
+    "revision": "f05ca2a732c60bcfc924",
+    "url": "/streams-redux-saga/static/js/4.1fa13f65.chunk.js"
+  },
+  {
+    "revision": "573c9a5ef33b14548752",
+    "url": "/streams-redux-saga/static/js/main.4db8f127.chunk.js"
+  },
+  {
+    "revision": "8afb1ad286cd9dac478d",
+    "url": "/streams-redux-saga/static/js/runtime~main.ced661d7.js"
+  }
+]);
